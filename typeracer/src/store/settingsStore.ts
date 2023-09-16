1b75162ff0376fbb0b12fd1e9bf5e31b41b0bc50
sanqro <contact@sanqro.xyz>
2023-09-16 15:39:14 +0200

    add testType to store
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

--- typeracer/src/store/settingsStore.ts
+++ typeracer/src/store/settingsStore.ts
@@ -2,9 +2,11 @@ import { writable } from 'svelte/store';
 
 const initialWordsCount = localStorage.getItem('wordsCount') || '10';
 const initialDuration = localStorage.getItem('duration') || '15';
+const initialTestType = localStorage.getItem('testType') || 'words';
 
 export const wordsCount = writable(initialWordsCount);
 export const duration = writable(initialDuration);
+export const testType = writable(initialTestType);
 
 export function updateWordsCount(newWordsCount: number | any) {
 	localStorage.setItem('wordsCount', newWordsCount);
@@ -12,4 +14,8 @@ export function updateWordsCount(newWordsCount: number | any) {
 
 export function updateDuration(newDuration: number | any) {
 	localStorage.setItem('duration', newDuration);
+}
+
+export function updateTestType(newTestType: string | any) {
+	localStorage.setItem('testType', newTestType);
 }
