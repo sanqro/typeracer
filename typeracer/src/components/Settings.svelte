feccbe85050f69d2a26858b7d479da72247b9494
sanqro <contact@sanqro.xyz>
2023-09-16 15:39:37 +0200

    update settings component to use testType
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

--- typeracer/src/components/Settings.svelte
+++ typeracer/src/components/Settings.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-	import { updateWordsCount, updateDuration, wordsCount, duration } from '../store/settingsStore';
-	import { onMount } from 'svelte';
+	import {
+		updateWordsCount,
+		updateDuration,
+		updateTestType,
+		wordsCount,
+		duration,
+		testType
+	} from '../store/settingsStore';
 
 	const handleWordsCountChange = () => {
 		if ($wordsCount !== undefined) {
@@ -13,40 +19,61 @@
 			updateDuration($duration);
 		}
 	};
+
+	const handeTestTypeChange = () => {
+		if ($testType !== undefined) {
+			updateTestType($testType);
+		}
+	};
 </script>
 
 <div class="container mx-auto p-4">
 	<h1 class="text-4xl mb-4">Settings</h1>
 	<form>
+		{#if $testType === 'words'}
+			<div class="mb-4">
+				<label for="wordsCount" class="block text-gray-700">Number of Words:</label>
+				<select
+					id="wordsCount"
+					bind:value={$wordsCount}
+					on:change={handleWordsCountChange}
+					class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+				>
+					<option value="10">10</option>
+					<option value="25">25</option>
+					<option value="50">50</option>
+					<option value="100">100</option>
+				</select>
+			</div>
+		{/if}
+		{#if $testType === 'time'}
+			<div class="mb-4">
+				<label for="duration" class="block text-gray-700"
+					>Duration of the typing test in seconds:</label
+				>
+				<select
+					id="duration"
+					bind:value={$duration}
+					on:change={handleDurationChange}
+					class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+				>
+					<option value="15">15</option>
+					<option value="30">30</option>
+					<option value="60">60</option>
+					<option value="120">120</option>
+				</select>
+			</div>
+		{/if}
 		<div class="mb-4">
-			<label for="wordsCount" class="block text-gray-700">Number of Words:</label>
+			<label for="testType" class="block text-gray-700">Type of the Test</label>
 			<select
-				id="wordsCount"
-				bind:value={$wordsCount}
-				on:change={handleWordsCountChange}
+				id="testType"
+				bind:value={$testType}
+				on:change={handeTestTypeChange}
 				class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 			>
-				<option value="10">10</option>
-				<option value="25">25</option>
-				<option value="50">50</option>
-				<option value="100">100</option>
-			</select>
-		</div>
-
-		<div class="mb-4">
-			<label for="duration" class="block text-gray-700"
-				>Duration of the typing test in seconds:</label
-			>
-			<select
-				id="duration"
-				bind:value={$duration}
-				on:change={handleDurationChange}
-				class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-			>
-				<option value="15">15</option>
-				<option value="30">30</option>
-				<option value="60">60</option>
-				<option value="120">120</option>
+				<option value="words">Number of Words</option>
+				<option value="time">Time</option>
 			</select>
 		</div>
 	</form>
