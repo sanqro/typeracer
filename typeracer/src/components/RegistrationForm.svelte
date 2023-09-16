e72cabaff673e4c746e4be6c9c662230ba544eb4
sanqro <contact@sanqro.xyz>
2023-09-16 22:50:18 +0200

    add registration form
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

---
+++ typeracer/src/components/RegistrationForm.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	let username = '';
+	let password = '';
+
+	const register = async () => {
+		try {
+			const response = await fetch('/register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ username, password })
+			});
+
+			const res = await response.json();
+			if (res.success) {
+				alert('Registered successfully!');
+			} else {
+				alert(res.error);
+			}
+		} catch (error: any) {}
+	};
+</script>
+
+<div>
+	<h1>Register</h1>
+	<form on:submit={register}>
+		<div>
+			<label for="username">Username</label>
+			<input type="text" placeholder="Username" id="username" bind:value={username} required />
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input type="password" placeholder="Password" id="password" bind:value={password} required />
+		</div>
+		<div>
+			<button type="submit">Register</button>
+		</div>
+	</form>
+</div>
