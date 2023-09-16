36156996d4834667a8d5a6219f25b70e90e8e484
sanqro <contact@sanqro.xyz>
2023-09-16 22:49:44 +0200

    add endpoint for registration
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

---
+++ typeracer/src/routes/register/+server.ts
@@ -0,0 +1,30 @@
+import { DETA_PROJECT_KEY } from '$env/static/private';
+import { Deta } from 'deta';
+import argon2 from 'argon2';
+import type { IRegisterForm } from './interfaces';
+import { json } from '@sveltejs/kit';
+
+const deta = Deta(DETA_PROJECT_KEY);
+const auth = deta.Base('users');
+
+export const POST = async ({ request }) => {
+	try {
+		const authFormData: IRegisterForm = (await request.json()) as IRegisterForm;
+		const user = await auth.get(authFormData.username);
+
+		if (user !== null) {
+			throw new Error('User already exists');
+		}
+
+		const passwordHash = await argon2.hash(authFormData.password);
+		const registerJsonData = {
+			key: authFormData.username,
+			password: passwordHash
+		};
+		const toInsert = await auth.insert(registerJsonData);
+
+		return json({ username: toInsert.key, success: true }, { status: 201 });
+	} catch (err: any) {
+		return json({ error: err.message, success: false }, { status: 503 });
+	}
+};
