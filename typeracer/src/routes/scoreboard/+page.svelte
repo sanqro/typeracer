74cb3f95fbe33d6978ff2db3e0d0cdbf06005e3b
sanqro <contact@sanqro.xyz>
2023-09-17 15:37:11 +0200

    add footer and header to scoreboard page

--- typeracer/src/routes/scoreboard/+page.svelte
+++ typeracer/src/routes/scoreboard/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Footer from '../../components/Footer.svelte';
+	import Header from '../../components/Header.svelte';
 	import Scoreboard from '../../components/Scoreboard.svelte';
 </script>
 
 <div class="p-4">
+	<Header />
 	<Scoreboard />
+	<Footer />
 </div>
