caaae048d0b3ad70ec8799adee21a6d35faa5125
sanqro <contact@sanqro.xyz>
2023-09-17 09:58:03 +0200

    add simple profile page with data
    Co-authored-by: RelxOff <doeme.kreisel@gmail.com>

--- typeracer/src/routes/+page.svelte
+++ typeracer/src/routes/+page.svelte
@@ -4,5 +4,6 @@
 </script>
 
 <div class="p-4">
+	<a href="/scoreboard">Scoreboard</a>
 	<TypingInput />
 </div>
---
+++ typeracer/src/routes/profile/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Profile from '../../components/Profile.svelte';
+</script>
+
+<div class="p-4">
+	<Profile />
+</div>
