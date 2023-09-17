2f8c151fc988d32013da14206d64815e49c73672
sanqro <contact@sanqro.xyz>
2023-09-17 15:35:15 +0200

    add Footer component

---
+++ typeracer/src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<footer class="bg-gray-800 text-white py-4">
+	<div class="container mx-auto text-center">
+		<p>
+			<a
+				href="https://github.com/sanqro/typeracer"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-white text-lg font-semibold hover:text-blue-300"
+			>
+				GitHub
+			</a>
+		</p>
+	</div>
+</footer>
