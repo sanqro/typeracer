16d4c50d41511509aa7e420c5205ca13131d13c0
sanqro <contact@sanqro.xyz>
2023-09-17 15:37:34 +0200

    add footer and header to settings page

--- typeracer/src/routes/settings/+page.svelte
+++ typeracer/src/routes/settings/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Footer from '../../components/Footer.svelte';
+	import Header from '../../components/Header.svelte';
 	import Settings from '../../components/Settings.svelte';
 </script>
 
 <div class="p-4">
+	<Header />
 	<Settings />
+	<Footer />
 </div>
