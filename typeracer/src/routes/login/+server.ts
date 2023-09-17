eb349d01856a7fe15e5dd01164475779ad67b9c9
sanqro <contact@sanqro.xyz>
2023-09-17 18:42:13 +0200

    rewrite login endpoint

--- typeracer/src/routes/login/+server.ts
+++ typeracer/src/routes/login/+server.ts
@@ -1,8 +1,7 @@
 import { DETA_PROJECT_KEY, JWT_SECRET } from '$env/static/private';
 import { Deta } from 'deta';
 import crypto from 'crypto-js';
-import { Buffer } from 'buffer/';
-import { createJWT } from '@sensethenlove/jwt';
+import { JWTPayload, SignJWT } from 'jose';
 import type { ILoginForm } from './interfaces';
 import { json } from '@sveltejs/kit';
 
@@ -21,9 +20,8 @@ export const POST = async ({ request }) => {
 		const passwordHash = crypto.SHA256(authFormData.password).toString(crypto.enc.Hex);
 
 		if (storedHash === passwordHash) {
-			const jwtPayload = { userId: 1 };
 			const expiresInAsSeconds = 32400;
-			const token = await createJWT(jwtPayload, expiresInAsSeconds, JWT_SECRET, Buffer);
+			const token = await createJWT(authFormData.username, expiresInAsSeconds, JWT_SECRET);
 			return json({ token: token, success: true }, { status: 201 });
 		} else {
 			return json({ message: 'Invalid Credentials', success: false }, { status: 401 });
@@ -32,3 +30,18 @@ export const POST = async ({ request }) => {
 		return json({ error: err.message, success: false }, { status: 503 });
 	}
 };
+
+async function createJWT(username: string, expiresIn: number, secret: string): Promise<string> {
+	const payload: JWTPayload = {
+		sub: username,
+		exp: Math.floor(Date.now() / 1000) + expiresIn
+	};
+
+	const secretUint8Array = new TextEncoder().encode(secret);
+
+	const token = await new SignJWT(payload)
+		.setProtectedHeader({ alg: 'HS256' })
+		.sign(secretUint8Array);
+
+	return token;
+}
