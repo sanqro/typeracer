f93ebde8df15f6c44d1843637eff44ac2191116f
sanqro <contact@sanqro.xyz>
2023-09-17 15:36:56 +0200

    add footer and header to register page

--- typeracer/src/routes/register/+page.svelte
+++ typeracer/src/routes/register/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Footer from '../../components/Footer.svelte';
+	import Header from '../../components/Header.svelte';
 	import RegistrationForm from '../../components/RegistrationForm.svelte';
 </script>
 
 <div class="p-4">
+	<Header />
 	<RegistrationForm />
+	<Footer />
 </div>
