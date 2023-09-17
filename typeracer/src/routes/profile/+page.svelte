090f838471e8bd45fb0e99f7cb92393b0a4c2be1
sanqro <contact@sanqro.xyz>
2023-09-17 23:17:24 +0200

    add footer and header to profile

--- typeracer/src/routes/profile/+page.svelte
+++ typeracer/src/routes/profile/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Footer from '../../components/Footer.svelte';
+	import Header from '../../components/Header.svelte';
 	import Profile from '../../components/Profile.svelte';
 </script>
 
 <div class="p-4">
+	<Header />
 	<Profile />
+	<Footer />
 </div>
